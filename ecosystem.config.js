98aedab1b84b21c781cf93e19a437f9974cd779f
DukeDeSouth <51200688+DukeDeSouth@users.noreply.github.com>
2025-06-28 22:52:34 -0400

    fix: Удален дефолтный JWT secret для выявления проблемы с переменными окружения

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'fonana',
-      script: './start-production.js',
+      script: './start-production-v2.js',
       instances: 1,
       autorestart: true,
       watch: false,
