a9f652e30a3093255083f8a501a737ecadac610c
DukeDeSouth <51200688+DukeDeSouth@users.noreply.github.com>
2025-07-02 08:54:24 -0700

    chore: update version to 20250702-085424-d422753

--- lib/version.ts
+++ lib/version.ts
@@ -1,4 +1,4 @@
 // This file is auto-generated during deployment
-export const APP_VERSION = '20250702-074727-36905f5'
-export const version = '20250702-074727-36905f5'
+export const APP_VERSION = '20250702-085424-d422753'
+export const version = '20250702-085424-d422753'
 export const buildDate = new Date().toISOString()
