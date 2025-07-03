3144f1af5964914cfe14511b7b49753144bc5275
root <root@vaults.cc>
2025-07-03 15:11:19 +0000

    chore: update version to 20250703-151119-816ec72

--- lib/version.ts
+++ lib/version.ts
@@ -1,4 +1,4 @@
 // This file is auto-generated during deployment
-export const APP_VERSION = '20250703-145506-7fb86e6'
-export const version = '20250703-145506-7fb86e6'
+export const APP_VERSION = '20250703-151119-816ec72'
+export const version = '20250703-151119-816ec72'
 export const buildDate = new Date().toISOString()
